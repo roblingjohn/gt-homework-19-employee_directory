b5d72dde45306a06d1daee998d4c014c12108f46
John Robling <roblingjohn@gmail.com>
2020-08-02 22:24:42 -0400

    added ability to sort by last name

--- src/components/Table.js
+++ src/components/Table.js
@@ -60,59 +60,65 @@ class Table extends Component {
   //   }
   // }
 
-
   componentDidMount() {
     this.generateEmployee();
+    this.sortEmployees();
   }
 
   state = {
-    array: []
-    }
-
+    employees: [],
+  };
 
   generateEmployee() {
     API.getRandomEmployee().then((res) => {
-      this.setState({array: res.data.results})
-    })
+      this.setState({ employees: res.data.results });
+    });
+  }
+
+  sortEmployees() {
+    let newArray = this.state.employees.sort((a, b) => (a.name.last >= b.name.last ? 1 : -1));
+    this.setState({ employees: newArray });
   }
 
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
-            <th>Cell number</th> 
-            <th>Age</th>
-            <th>Street address</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip</th>
-
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.array.map(employee => (
-
-          
-          <Employee 
-          first={employee.name.first}
-          last={employee.name.last}
-          email={employee.email}
-          cell={employee.cell}
-          age={employee.dob.age}
-          address={employee.location.street.number + " " + employee.location.street.name} 
-          city={employee.location.city}
-          state={employee.location.state}
-          zip={employee.location.postcode}
-
-          />
-          ))}
-
-        </tbody>
-      </table>
+      <div>
+        <button>Sort</button>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th onClick={() => {this.sortEmployees()}}>Last Name</th>
+              <th>Email</th>
+              <th>Cell number</th>
+              <th>Age</th>
+              <th>Street address</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zip</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map((employee) => (
+              <Employee
+                first={employee.name.first}
+                last={employee.name.last}
+                email={employee.email}
+                cell={employee.cell}
+                age={employee.dob.age}
+                address={
+                  employee.location.street.number +
+                  " " +
+                  employee.location.street.name
+                }
+                city={employee.location.city}
+                state={employee.location.state}
+                zip={employee.location.postcode}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
