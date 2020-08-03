0981f3912a6968bed4af7f2d4ac52ed3bafcfc89
John Robling <roblingjohn@gmail.com>
2020-08-03 17:02:25 -0400

    changed number of results in API call to 100

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default {
   getRandomEmployee: function() {
-    return axios.get("https://randomuser.me/api/?nat=us&results=30");
+    return axios.get("https://randomuser.me/api/?nat=us&results=100");
   },
 };
 
